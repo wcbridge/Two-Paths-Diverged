50c22487bd0f2aafc29877d356c6a9f8dc7d066a
wcbridge <33330345+wcbridge@users.noreply.github.com>
2020-07-22 15:57:20 -0400

    new outcomes

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let noun = ["You hear a sound through the trees. It is getting closer. You feel fear in your soul. The soul that you will soon lose to the aproaching evil.", "The path is gone. You turn around to see that you are no longer familiar with where you are. You will never escape this wood."]
+let noun = ["You hear a sound through the trees. It is getting closer. You feel fear in your soul. The soul that you will soon lose to the aproaching evil.", "The path is gone. You turn around to see that you are no longer familiar with where you are. You will never escape this wood.", "THE EVIL! OH GOD THE EVIL!!!", "Your last words: Oh No!",
+"The sky grows dark. It is coming closer. The dark sky itself is coming closer. Soon you will feel it's embrace, as your reality fades","Why are you here? You can not remember. How did you get here? Will anyone come to find you? Will you starve?"]
 
 
 
